0c5193b8f6295590090663e5cc44752c37d4759a
MarceloSouza <souza.marcelogi@gmail.com>
2021-09-29 12:38:26 -0300

    Delete Extract_Cliente.sql

--- Scripts-Extract/Extract_Cliente.sql
+++
@@ -1,8 +0,0 @@
-
---- Extraindo dados de Vendedor da Fonte/Source
---- Fonte: Banco de dados Firebird
-
-SELECT u."CODIGO" , u.NOME AS NOME_VENDEDOR, f.COMISSAOVISTA , f.COMISSAOPRAZO FROM USUARIOS u 
-INNER JOIN FUNCIONARIOS f ON 
-u.VENDEDOR_VINC = f."CODIGO" ;
-
