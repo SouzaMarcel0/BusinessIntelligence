a783cc41713cb4d91597617f0cb9c229766d3b58
MarceloSouza <souza.marcelogi@gmail.com>
2021-09-29 12:38:36 -0300

    Create Extract_Clientes.sql

---
+++ Scripts-Extract/Extract_Clientes.sql
@@ -0,0 +1,19 @@
+
+--- Extraindo dados de Clientes da Fonte/Source
+--- Fonte: Banco de dados Firebird
+
+SELECT 
+"CODIGO" , 
+NOME , 
+TIPOPESSOA , 
+"CPF" , 
+"CNPJ" ,
+CLASSE, 
+LIM_CRED , 
+BAIRRO , 
+CIDADE , 
+"UF" , 
+DATACADASTRO , 
+ULTIMAVENDA 
+FROM CLIENTES c ;
+
