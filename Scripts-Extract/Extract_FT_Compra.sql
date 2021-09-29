4bff5da907d050dfab16d29eac36598068283646
MarceloSouza <souza.marcelogi@gmail.com>
2021-09-29 14:19:48 -0300

    Create Extract_FT_Compra.sql

---
+++ Scripts-Extract/Extract_FT_Compra.sql
@@ -0,0 +1,20 @@
+
+--- Extraindo dados de Fornecedor da Fonte/Source
+--- Fonte: Banco de dados Firebird
+
+SELECT 
+c2."DATA" ,
+c.CODIGOMESTRE ,
+c2.FORNECEDOR,
+c.PRODUTO,
+QUANTIDADE ,
+UNITARIO ,
+DESCONTO ,
+ACRESCIMO ,
+SUBTOTAL
+FROM COMPRADETALHE c 
+inner JOIN COMPRAMESTRE c2 ON 
+c.CODIGOMESTRE = c2.CODIGOMESTRE 
+where "DATA" >= '2018-01-01';
+
+-- OBS: Data de corte para extração dos dados definimos aqui.
